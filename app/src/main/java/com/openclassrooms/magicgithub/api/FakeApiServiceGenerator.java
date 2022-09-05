3e8e692ac0aede19f579d59491be6cb8ef6fba42
Jérémy Mulet <mulet.jeremy@gmail.com>
2022-09-05 20:54:06 +0200

    Fix users avatar glide api wasn't working

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.java
@@ -15,33 +15,33 @@ public abstract class FakeApiServiceGenerator {
     }
 
     public static List<User> FAKE_USERS = Arrays.asList(
-            new User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-            new User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-            new User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-            new User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
-            new User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-            new User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
-            new User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-            new User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
-            new User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-            new User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
-            new User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-            new User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-            new User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
-            new User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-            new User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-            new User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-            new User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-            new User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-            new User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-            new User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+            new User("001", "Jake", "https://i.pravatar.cc/400?img=1"),
+            new User("002", "Paul", "https://i.pravatar.cc/400?img=2"),
+            new User("003", "Phil", "https://i.pravatar.cc/400?img=3"),
+            new User("004", "Guillaume", "https://i.pravatar.cc/400?img=4"),
+            new User("005", "Francis", "https://i.pravatar.cc/400?img=5"),
+            new User("006", "George", "https://i.pravatar.cc/400?img=6"),
+            new User("007", "Louis", "https://i.pravatar.cc/400?img=7"),
+            new User("008", "Mateo", "https://i.pravatar.cc/400?img=8"),
+            new User("009", "April", "https://i.pravatar.cc/400?img=9"),
+            new User("010", "Louise", "https://i.pravatar.cc/400?img=10"),
+            new User("011", "Elodie", "https://i.pravatar.cc/400?img=11"),
+            new User("012", "Helene", "https://i.pravatar.cc/400?img=12"),
+            new User("013", "Fanny", "https://i.pravatar.cc/400?img=13"),
+            new User("014", "Laura", "https://i.pravatar.cc/400?img=14"),
+            new User("015", "Gertrude", "https://i.pravatar.cc/400?img=15"),
+            new User("016", "Chloé", "https://i.pravatar.cc/400?img=16"),
+            new User("017", "April", "https://i.pravatar.cc/400?img=17"),
+            new User("018", "Marie", "https://i.pravatar.cc/400?img=18"),
+            new User("019", "Henri", "https://i.pravatar.cc/400?img=19"),
+            new User("020", "Rémi", "https://i.pravatar.cc/400?img=20")
     );
 
     public static List<User> FAKE_USERS_RANDOM = Arrays.asList(
-            new User("021", "Lea", "https://api.adorable.io/AVATARS/512/21.png"),
-            new User("022", "Geoffrey", "https://api.adorable.io/AVATARS/512/22.png"),
-            new User("023", "Simon", "https://api.adorable.io/AVATARS/512/23.png"),
-            new User("024", "André", "https://api.adorable.io/AVATARS/512/24.png"),
-            new User("025", "Leopold", "https://api.adorable.io/AVATARS/512/25.png")
+            new User("021", "Lea", "https://i.pravatar.cc/400?img=21"),
+            new User("022", "Geoffrey", "https://i.pravatar.cc/400?img=22"),
+            new User("023", "Simon", "https://i.pravatar.cc/400?img=23"),
+            new User("024", "André", "https://i.pravatar.cc/400?img=24"),
+            new User("025", "Leopold", "https://i.pravatar.cc/400?img=25")
     );
 }
